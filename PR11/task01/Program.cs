ce4b0537fcbb57bb400ee9447d86dc5a787210be
MidLut16 <73688048+MidLut16@users.noreply.github.com>
2020-10-31 21:40:42 +0300

    Add files via upload

---
+++ PR11/task01/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //1. Из пяти целых положительных чисел найти два наибольших.
+        {
+            {
+                int max = 0;
+                int max2 = 0;
+
+                Console.WriteLine("Введите первое число: ");
+                int C1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите второе число: ");
+                int C2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите третье число: ");
+                int C3 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите четвертое число: ");
+                int C4 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите пятое число: ");
+                int C5 = Convert.ToInt32(Console.ReadLine());
+
+                if (max < C1)
+                {
+                    max = C1;
+                }
+                if (max < C2)
+                {
+                    max = C2;
+                }
+                if (max < C3)
+                {
+                    max = C3;
+                }
+                if (max < C4)
+                {
+                    max = C4;
+                }
+                if (max < C5)
+                {
+                    max = C5;
+                }
+
+                if (max != C1 && max2 < C1)
+                {
+                    max2 = C1;
+                }
+                if (max != C2 && max2 < C2)
+                {
+                    max2 = C2;
+                }
+                if (max != C3 && max2 < C3)
+                {
+                    max2 = C3;
+                }
+                if (max != C4 && max2 < C4)
+                {
+                    max2 = C4;
+                }
+                if (max != C5 && max2 < C5)
+                {
+                    max2 = C5;
+                }
+
+                Console.WriteLine("Первое максимальное число: {0} \n Второе максимальное число: {1} ", max, max2);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ PR11/task02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c1 = 0;
+            int c2 = 0;
+            int c3 = 0;
+            int c4 = 0;
+
+            Console.WriteLine("Введите четырехзначное число (>0):");
+            int chis = Convert.ToInt32(Console.ReadLine());
+
+            c1 = chis / 1000;
+            chis = chis - c1 * 100;
+
+            c2 = chis / 100;
+            chis = chis - c2 * 10;
+
+            c3 = chis / 10;
+            chis = chis - c3 * 10;
+
+            c4 = chis;
+
+            if (c1 != c2 && c2 != c3 && c3 != c1 && c1 != c4 && c2 != c4 && c3 != c4)
+            {
+                Console.WriteLine("Высказывание верное!");
+            }
+            else
+            {
+                Console.WriteLine("Высказывание неверное!");
+            }
+        }
+    }
+}
